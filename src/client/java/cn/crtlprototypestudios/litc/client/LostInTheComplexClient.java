0daf13cd6713e5aa257504cf8b78c72ac939f931
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-08-07 11:55:45 +0800

    Init Commit

---
+++ src/client/java/cn/crtlprototypestudios/litc/client/LostInTheComplexClient.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios.litc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LostInTheComplexClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/crtlprototypestudios/litc/LostInTheComplex.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios.litc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LostInTheComplex implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
