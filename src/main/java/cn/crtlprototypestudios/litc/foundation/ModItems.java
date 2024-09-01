3908654cfa3589f9e50e84c0bbe94d6282e9bd23
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-09-01 17:23:16 +0800

    i

--- src/main/java/cn/crtlprototypestudios/litc/foundation/ModItems.java
+++ src/main/java/cn/crtlprototypestudios/litc/foundation/ModItems.java
@@ -3,8 +3,13 @@ package cn.crtlprototypestudios.litc.foundation;
 import cn.crtlprototypestudios.litc.utility.RegistryHelper;
 import cn.crtlprototypestudios.litc.utility.RegistryEntry;
 import net.minecraft.item.Item;
+import net.minecraft.util.Rarity;
 
 public class ModItems {
+    public static final RegistryEntry<Item> PLANKS = RegistryHelper.item("planks")
+            .settings(settings -> settings.maxCount(40).rarity(Rarity.COMMON))
+            .build();
+
     public static final RegistryEntry<Item> CHOCOLATE_BAR = RegistryHelper.item("chocolate_bar")
             .settings(settings -> settings.food(ModComponents.CHOCOLATE_BAR_COMPONENT))
             .build();
